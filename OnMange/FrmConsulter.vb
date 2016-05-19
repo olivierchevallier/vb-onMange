1ad0ba8bf70fa1e3eae300d8971f967b250dd18b
olivierchevallier <olivierchevallier@macbook-pro-dolivier.local>
2016-05-19 15:52:11 +0200

    Ajout de la fonction de recherche dans le formulaire de consultation

--- OnMange/FrmConsulter.vb
+++ OnMange/FrmConsulter.vb
@@ -4,13 +4,32 @@
     End Sub
 
     Private Sub FrmConsulter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        afficherPlats()
+        afficherPlats(listePlats)
     End Sub
 
-    Private Sub afficherPlats()
+    Private Sub afficherPlats(ByVal platsAAfficher As List(Of Plat))
         dgvHistorique.Rows.Clear()
-        For i = 0 To listePlats.Count - 1
-            dgvHistorique.Rows.Add(listePlats(i).ToStringArray())
+        For i = 0 To platsAAfficher.Count - 1
+            dgvHistorique.Rows.Add(platsAAfficher(i).ToStringArray())
         Next
+    End Sub
+
+    Private Sub cmdRechercher_Click(sender As Object, e As EventArgs) Handles cmdRechercher.Click
+        Dim origine As String, pasMange As Date
+        origine = If(IsNothing(cboMangerOrigine.SelectedItem), "", cboMangerOrigine.SelectedItem)
+        pasMange = Date.Now
+        Select Case cboPasMange.SelectedItem
+            Case "Un jour"
+                pasMange = pasMange.AddDays(-1)
+            Case "Cinq jours"
+                pasMange = pasMange.AddDays(-5)
+            Case "Une semaine"
+                pasMange = pasMange.AddDays(-7)
+            Case "Deux semaines"
+                pasMange = pasMange.AddDays(-14)
+            Case "Un mois"
+                pasMange = pasMange.AddMonths(-1)
+        End Select
+        afficherPlats(RechercherPlats(origine, pasMange))
     End Sub
 End Class
